ccdaacb759fb2e33ef230506ce55ba3fa01839bc
mstrieleman <mstrieleman@gmail.com>
2018-03-05 14:19:13 -0800

    Refactored code for overall readability.

--- main.js
+++ main.js
@@ -1,66 +1,44 @@
-/* eslint-disable no-unused-vars */
-document.addEventListener("keypress", event => {
-  letterCheck();
+document.addEventListener('keypress', event => {
+  letterCheck(event.key);
 });
 
-Array.prototype.random = function() {
-  return this[Math.floor(Math.random() * this.length)];
-};
-
-function updateContent(value) {
-  document.querySelector(".letter").textContent = value;
+function randomWord(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
+function updateLetter(letter, element) {
+  element.textContent = letter;
 }
 
-function updateScore(value) {
-  document.querySelector(".score").textContent = value;
-}
-
-function displayLetter(i) {
-  returnLetter = splitArray.slice(0)[i];
-  return returnLetter;
-}
-
-function toString(arr) {
-  arr.toString();
-  return arr;
+function updateScore(score, element) {
+  element.textContent = score;
 }
 
 function initContent() {
   document.body.onload = initContent;
-  document.querySelector(".entire-word").textContent = getRandom;
-  document.querySelector(".letter").textContent = displayLetter(0, 0);
+  document.querySelector('.entire-word').textContent = letters.join('');
+  document.querySelector('.letter').textContent = letters[0];
 }
 
-function currentLetter(i) {
-  var letter = toString(displayLetter(i));
-  return letter;
-}
-
-function letterCheck() {
-  const keyName = event.key;
-  eventLetter = currentLetter(position);
-  if (run < splitArrayLength) {
-    if (event.key === eventLetter) {
-      run = run + 1;
+function letterCheck(letter) {
+  if (position < letters.length - 1) {
+    if (letter === letters[position]) {
       score = score + 1;
       position = position + 1;
-      updateContent(toString(displayLetter(position)));
-      updateScore(score);
-    } else {
-      alert("WRONG LETTER BRUH");
+      updateLetter(letters[position], document.querySelector('.letter'));
+      updateScore(score, document.querySelector('.score'));
     }
-  } else {
-    alert("YOU DID IT BRUH");
+    else {
+      alert('WRONG LETTER BRUH');
+    }
+  }
+  else {
+    alert('YOU DID IT BRUH');
     location.reload();
   }
 }
 
 var position = 0;
-var words = ["Boustrophedon", "Bowyang", "Fartlek", "Rapscallion", "Vomitory"];
-var getRandom = words.random();
-var splitArray = getRandom.split("");
-var splitArrayLength = splitArray.length;
-var run = 1;
+var words = ['Boustrophedon', 'Bowyang', 'Fartlek', 'Rapscallion', 'Vomitory'];
+var letters = randomWord(words).split('');
 var score = 0;
-
 initContent();
