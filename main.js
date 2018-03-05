7f372cc025f9fd207ba4918d163182687eb5c123
mstrieleman <mstrieleman@gmail.com>
2018-03-05 10:56:56 -0800

    Submitting feature for user feedback.

--- main.js
+++ main.js
@@ -1,26 +1,67 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
+document.addEventListener("keypress", event => {
+  letterCheck();
+});
 
 Array.prototype.random = function() {
   return this[Math.floor(Math.random() * this.length)];
 };
 
-var words = [
-  "Boustrophedon",
-  "Bowyang",
-  "Fartlek",
-  "Rapscallion",
-  "Vomitory"
-];
-
-var getRandom = words.random();
-var splitArray = getRandom.split("");
-var returnFirst = splitArray.slice(0)[0]
-
-function print() {
-  document.body.onload = print;
-  document.querySelector(".entire-word").textContent = getRandom;
-  document.querySelector(".first-letter").textContent = returnFirst;
+function updateContent(value) {
+  document.querySelector(".letter").textContent = value;
 }
 
-print();
+function updateScore(value) {
+  document.querySelector(".score").textContent = value;
+}
+
+function displayLetter(i) {
+  returnLetter = splitArray.slice(0)[i];
+  return returnLetter;
+}
+
+function toString(arr) {
+  arr.toString();
+  return arr;
+}
+
+function initContent() {
+  document.body.onload = initContent;
+  document.querySelector(".entire-word").textContent = getRandom;
+  document.querySelector(".letter").textContent = displayLetter(0, 0);
+}
+
+function currentLetter(i) {
+  var letter = toString(displayLetter(i));
+  return letter;
+}
+
+function letterCheck() {
+  const keyName = event.key;
+  eventLetter = currentLetter(position);
+  if (run < splitArrayLength) {
+    console.log(run);
+    if (event.key === eventLetter) {
+      run = run + 1;
+      score = score + 1;
+      position = position + 1;
+      updateContent(toString(displayLetter(position)));
+      updateScore(score);
+    } else {
+      alert("WRONG LETTER BRUH");
+    }
+  } else {
+    alert("YOU DID IT BRUH");
+    location.reload();
+  }
+}
+
+var position = 0;
+var words = ["Boustrophedon", "Bowyang", "Fartlek", "Rapscallion", "Vomitory"];
+var getRandom = words.random();
+var splitArray = getRandom.split("");
+var splitArrayLength = splitArray.length;
+var run = 1;
+var score = 0;
+
+initContent();
